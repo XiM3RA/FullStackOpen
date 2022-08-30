0424dc7a4c9edf9d1bc2b83611b93878f686e4b3
XiM3RA <ddigler@protonmail.com>
2022-08-30 14:04:51 -0500

    unicafe 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,46 @@
 import { useState } from 'react'
 
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+)
+
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [total, setTotal] = useState(0)
+    const [score, setScore] = useState(0)
+    
+    const handleGoodClick = () => {
+        setGood(good+1)
+        setTotal(total+1)
+        setScore(score+1)
+    }
+    const handleNeutralClick = () => {
+        setNeutral(neutral+1)
+        setTotal(total+1)
+    }
+    const handleBadClick = () => {
+        setBad(bad+1)
+        setTotal(total+1)
+        setScore(score-1)
+    }
 
     return (
         <div>
-          code here
+          <h1>give feedback</h1>
+          <Button handleClick={handleGoodClick} text="good" />
+          <Button handleClick={handleNeutralClick} text="neutral" />
+          <Button handleClick={handleBadClick} text="bad" />
+          <h1>statistics</h1>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {total}</p>
+          <p>average {score / total}</p>
+          <p>positive {good / total * 100}</p>
         </div>
     )
 }
