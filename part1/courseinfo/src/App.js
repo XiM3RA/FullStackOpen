c0b58f2b9b847daf389b79bc562a0bf106ffdc4e
XiM3RA <ddigler@protonmail.com>
2022-08-30 12:28:27 -0500

    Start of unicafe

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,52 +1,49 @@
-const App = () => {
-    const course = { 
-    name: 'Half Stack application development',
-    parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-}
+import { useState } from 'react'
 
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
+    }
     return (
         <div>
-            <Header course={course} />
-            <Content course={course} />
-            <Total course={course} />
+            button press history: {props.allClicks.join(' ')}
         </div>
     )
 }
 
-const Header = (props) => {
-    return (
-        <h1>{props.course.name}</h1>
-    )
-}
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+)
 
-const Content = (props) => {
-    return (
-        <>
-        <p>{props.course.parts[0].name} {props.course.parts[0].exercises}</p>
-        <p>{props.course.parts[1].name} {props.course.parts[1].exercises}</p>
-        <p>{props.course.parts[2].name} {props.course.parts[2].exercises}</p>
-        </>
-    )
-}
+const App = () => {
+    const [ left, setLeft ] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
 
-const Total = (props) => {
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left+1)
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right+1)
+    }
+
     return (
-        <p>Number of exercises {props.course.parts[0].exercises +
-                                props.course.parts[1].exercises +
-                                props.course.parts[2].exercises }</p>
+        <div>
+            {left}
+            <Button handleClick={handleLeftClick} text="left" />
+            <Button handleClick={handleRightClick} text="right" />
+            {right}
+            <History allClicks={allClicks} />
+        </div>
     )
 }
 
