acf89f41ed85d846a7e2c2453dfb5ab9b9f275ad
XiM3RA <ddigler@protonmail.com>
2022-08-31 20:30:04 -0500

    1.12.js

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,12 @@
 import { useState } from 'react'
 
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+    {text}
+    </button>
+)
+
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -10,12 +17,17 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
+
+  const handleClick = () => {
+      setSelected(Math.floor(Math.random() * 7))
+  }
    
   const [selected, setSelected] = useState(0)
 
   return (
     <div>
-      {anecdotes[selected]}
+      <p>{anecdotes[selected]}</p>
+      <Button handleClick={handleClick} text="next anecdote" />
     </div>
   )
 }
