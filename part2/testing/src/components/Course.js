abe4326238288cb50bdc44d39645c3c1885eb0b1
XiM3RA <ddigler@protonmail.com>
2022-09-02 21:28:38 -0500

    2.5 & 2.6

--- part2/testing/src/components/Course.js
+++ part2/testing/src/components/Course.js
@@ -1,8 +1,46 @@
 import React from 'react'
 
-const Course = ({ name, exercises }) => {
+const Header = ({ course, key }) => {
     return (
-        <p>{name} {exercises}</p>
+        <h2>{course.name}</h2>
+    )
+}
+
+const Total = ({ course }) => {
+    const sum = course.parts.reduce((partialSum, a) => partialSum + a.exercises, 0)
+
+    return (
+        <p><strong>total of {sum} exercises</strong></p>
+    )
+}
+
+const Line = ( props ) => {
+    return (
+        <p>
+        {props.part.name} {props.part.exercises}
+        </p>    
+    )
+}
+
+const Body = ({ course }) => {
+    return (
+        <>
+        {course.parts.map((part) => 
+            <Line part={part} key={part.id} />
+        )
+        }
+        </>
+    )
+}
+
+
+const Course = ({ course }) => {
+    return (
+        <>
+        <Header course={course} />
+        <Body course={course} />
+        <Total course={course} />
+        </>
     )
 }
 
