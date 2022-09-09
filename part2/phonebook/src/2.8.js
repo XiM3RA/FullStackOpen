820e39ec7787ba611ef3810341438ad6c323691b
XiM3RA <ddigler@protonmail.com>
2022-09-09 10:16:12 -0500

    Added 2.9.js, phonebook has a simple search mechanism

--- part2/phonebook/src/2.8.js
+++ part2/phonebook/src/2.8.js
@@ -4,21 +4,11 @@ import Note from "./components/Note";
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumb, setNewNumb] = useState("");
-  const [searchFunc, setSearchFunc] = useState("");
 
-  // const namesToShow = persons
-  const namesToShow =
-    searchFunc === ""
-      ? persons
-      : persons.filter((name) =>
-          name.name.toLowerCase().includes(searchFunc.toLowerCase())
-        );
+  const namesToShow = persons;
 
   const addName = (event) => {
     event.preventDefault();
@@ -47,25 +37,9 @@ const App = () => {
     setNewNumb(event.target.value);
   };
 
-  const handleSearchFunc = (event) => {
-    setSearchFunc(event.target.value);
-  };
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          filter by name{" "}
-          <input
-            id="filter"
-            name="filter"
-            value={searchFunc}
-            onChange={handleSearchFunc}
-          />
-        </div>
-        <h2>Add new entry</h2>
-      </form>
       <form onSubmit={addName}>
         <div>
           name:{" "}
