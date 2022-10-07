0ff9f368f6d3cff8422a471911e8d4ad5319b0b8
XiM3RA <ddigler@protonmail.com>
2022-10-07 14:29:45 -0500

    3.4, added method to delete entry from phonebook. Only checks if delete request has an id

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,57 +1,68 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 app.use(express.json());
 
 let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    },
-    {
-        id: 5,
-        name: "New Person",
-        number: "555-5555"
-    },
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122",
+  },
+  {
+    id: 5,
+    name: "New Person",
+    number: "555-5555",
+  },
+];
 
 app.get("/info", (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length}  people</p>
+  res.send(`<p>Phonebook has info for ${persons.length}  people</p>
               <p>${new Date()}</p>`);
 });
 
 app.get("/api/persons", (req, res) => {
-    res.json(persons)
+  res.json(persons);
 });
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find((person) => person.id === id);
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
 
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons", (request, response) => {
+  const body = request.body;
+  if (body.id) {
+    let temp = persons.filter((i) => i.id != body.id);
+    persons = temp;
+    response.json(temp);
+  } else {
+    response.status(404).json({ error: "invalid id" });
+  }
 });
 
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
