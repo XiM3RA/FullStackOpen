703fc8c5607ada6eef668208c6dac4ed257ff6b9
XiM3RA <ddigler@protonmail.com>
2023-02-23 09:25:55 -0600

    Created mongo.js for adding and searching a phonebook database for task 3.12

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,4 +1,9 @@
+// Usage: node mongo.js <Mongo password> <"Person Name"> <number> -> Adds entry
+// Or: node mongjo.js <Mongo password> -> returns the list of entries
+
 const mongoose = require('mongoose')
+
+function main() {
 
 if (process.argv.length < 3) {
     console.log('give password as an argument')
@@ -8,31 +13,40 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 
 const url = 
-    `mongodb+srv://fullstack:${password}@cluster0.zcssndw.mongodb.net/noteApp?retryWrites=true&w=majority`
+    `mongodb+srv://fullstack:${password}@cluster0.zcssndw.mongodb.net/phonebook?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
+// Define the schema used
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+    name: String,
+    phoneNumber: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Entry = mongoose.model('Entry', noteSchema)
 
-//const note = new Note({
-//    content: 'HTML is Easy',
-//    important: true,
-//})
-
-//note.save().then(result => {
-//    console.log('note saved!')
-//    mongoose.connection.close()
-//})
-
-Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
+if (process.argv.length == 3) {
+    console.log("phonebook:")
+    Entry.find({}).then(result => {
+        result.forEach(entry => {
+            console.log(entry.name, entry.phoneNumber)
+        })
+        mongoose.connection.close()
     })
+    return;
+    }
+
+const entry = new Entry({
+    name: process.argv[3],
+    phoneNumber: process.argv[4],
+})
+
+entry.save().then(result => {
+    console.log('phone number saved!')
     mongoose.connection.close()
 })
+}
+
+main();
+
