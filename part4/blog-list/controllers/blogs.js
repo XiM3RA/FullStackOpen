61237e8751e0179f2f2362531e3c20fa9e63ed3f
XiM3RA <ddigler@protonmail.com>
2023-03-14 15:34:34 -0500

    Section 4 complete, not all * yet

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require("express").Router();
+const jwt = require('jsonwebtoken');
 const Blog = require("../models/blog");
 const User = require('../models/user');
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 blogsRouter.get("/", async (request, response) => {
   //    Blog.find({}).then(blogs => {
@@ -29,8 +38,12 @@ blogsRouter.get("/:id", async (request, response) => {
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+  }
 
-  const user = await User.findById(body.user);
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
