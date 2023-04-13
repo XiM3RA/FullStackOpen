62c34d3288f87107d3090584712cef5f7a03c2e0
XiM3RA <ddigler@protonmail.com>
2023-04-12 19:07:43 -0500

    Ex 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,32 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [toggle, flipToggle] = useState(true);
+
+  const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+  }
+
+    return (
+        <>
+        { toggle ?
+            <div style={blogStyle}>
+                {blog.title} {blog.author}
+                <button onClick={() => flipToggle(!toggle)} type="text">view</button>
+            </div> :
+            <div style={blogStyle}>
+            {blog.title} {blog.author} <button onClick={() => flipToggle(!toggle)} type="text">hide</button><br></br>
+            {blog.url}<br></br>
+            likes: {blog.likes} <button type="text">like</button><br></br>
+            {blog.user.name}
+            </div>}
+
+        </>
+    )
+}
 
 export default Blog
